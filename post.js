feaf5677f7239972c5977b322b699f2e89b34a1a
Cee <cee@chu2byo.com>
2015-06-15 18:40:13 +0800

    Format Code

--- post.js
+++ post.js
@@ -2,17 +2,14 @@ var request = require('request');
 var username = ;
 var password = '';
 
-request.post(
-	{
-		url: 'http://p.nju.edu.cn/portal_io/login',
-		form: {
-			username: username,
-			password: password
-		}
-	},
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-    }
-);
+request.post({
+	url: 'http://p.nju.edu.cn/portal_io/login',
+	form: {
+		username: username,
+		password: password
+	}
+}, function(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		console.log(body)
+	}
+});
